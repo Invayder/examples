192fc3410bf776a231de4d02416b3f3c300d0653
Invayder <codmod99@gmail.com>
2013-12-16 15:39:31 -0500

    Added JFrame.

--- Examples/src/examples/main/aa.java
+++ Examples/src/examples/main/aa.java
@@ -1,5 +1,17 @@
 package examples.main;
 
-public class aa {
+import javax.swing.*;
 
+public class aa {
+	public static void main(String[] args) {
+		// Creating Objects \\
+		JFrame frame = new JFrame();
+		// Frame Settings \\
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setSize(200, 60);
+		frame.setTitle("Example JFrame!");
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
